fcf8bd9cdb6479ffce04b9730239ca88e2c61b5f
Simplr-Abner <lvzhenbin@hotmail.com>
2015-10-11 02:30:30 +0800

    add my_task function add servlet

---
+++ src/com/lzb/oa/dao/SettingDAO.java
@@ -0,0 +1,66 @@
+package com.lzb.oa.dao;
+
+import java.io.IOException;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import com.alibaba.fastjson.JSONArray;
+import com.lzb.oa.bean.RoomerInfo;
+import com.lzb.oa.util.JsonUtil;
+
+public class SettingDAO {
+	private DBManager manager;
+	private static SettingDAO dao;
+
+	public static SettingDAO getInstance() throws ClassNotFoundException,
+			IOException {
+		if (dao == null) {
+			dao = new SettingDAO();
+		}
+		return dao;
+	}
+	
+	
+	public SettingDAO() throws ClassNotFoundException, IOException{
+		manager = DBManager.getInstance();
+	}
+	
+	public JSONArray getMyTask(String emp_no) {
+		String sql = "select roomer_info.*,house_city, house_address from roomer_info, house_info where roomer_info.roomer_house_no = house_info.house_no and roomer_emp_no = '"+emp_no
+				+"' order by roomer_date desc, roomer_period asc";
+		
+		JSONArray tasks = new JSONArray();
+		try {
+			
+			manager.connDB();
+			ResultSet rs = manager.executeQuery(sql);
+			while(rs.next()){
+				
+				String roomer_no = rs.getString("roomer_no");
+				String roomer_name = rs.getString("roomer_name");
+				String roomer_sex = rs.getString("roomer_sex");
+				String roomer_phone_no = rs.getString("roomer_phone_no");
+				String roomer_house_no = rs.getString("roomer_house_no");
+				String roomer_date = rs.getDate("roomer_date").toString();
+				String roomer_period = rs.getString("roomer_period");
+				String roomer_rent = rs.getString("roomer_rent");
+				String roomer_complete = rs.getString("roomer_complete");
+				String roomer_emp_no = rs.getString("roomer_emp_no");
+				String house_city = rs.getString("house_city");
+				String house_address = rs.getString("house_address");
+				RoomerInfo rInfo = new RoomerInfo(roomer_no, roomer_name, roomer_sex, roomer_phone_no, null, roomer_house_no,
+						roomer_date, roomer_period, roomer_rent, roomer_complete, roomer_emp_no==null?"":roomer_emp_no);
+				rInfo.setHouse_address(house_address);
+				rInfo.setHouse_city(house_city);
+				String json = JsonUtil.createJsonString(rInfo);
+				System.out.println(json);
+				tasks.add(json);	
+			}
+			manager.closeDB();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return tasks;
+	}
+
+}
---
+++ src/com/lzb/oa/servlet/setting/MyTaskServlet.java
@@ -0,0 +1,50 @@
+package com.lzb.oa.servlet.setting;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
+import com.lzb.oa.dao.SettingDAO;
+import com.lzb.oa.dao.TaskManagerDAO;
+import com.lzb.oa.servlet.BaseServlet;
+
+/**
+ * Servlet implementation class MyTaskServlet
+ */
+@WebServlet(name="/MyTaskServlet",urlPatterns="/my_task.json")
+public class MyTaskServlet extends BaseServlet {
+	private static final long serialVersionUID = 1L;
+       
+    
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		JSONObject jsonObj = getClientJSON(request);
+		String emp_no = jsonObj.getString("emp_no");
+		System.out.println("emp_no = " + emp_no);
+		JSONArray tasks = null;
+		try {
+			tasks = SettingDAO.getInstance().getMyTask(emp_no);
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+		response.setContentType("text/html;charset=utf-8");
+		 //页面输出JSONArray的内容  
+        PrintWriter out = response.getWriter();  
+        out.print(tasks);  
+        out.flush();
+        out.close();
+	}
+
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request,response);
+	}
+
+}
